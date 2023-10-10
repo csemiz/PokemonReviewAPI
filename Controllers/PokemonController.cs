3408cef7e95c7ba94aa78e67c2aa70dd35ab4356
csemiz <ceren_semiz@outlook.com>
2023-10-10 18:30:14 +0300

    Add project files.

---
+++ Controllers/PokemonController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewAPI.Data;
+using PokemonReviewAPI.Dto;
+using PokemonReviewAPI.Interfaces;
+using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        private readonly DataContext _context;
+
+        public PokemonController(IPokemonRepository pokemonRepository, DataContext context)
+        {
+            _pokemonRepository = pokemonRepository;
+            _context = context;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult GetPokemons()
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+
+        [HttpGet("{pokeId}")]
+        [ProducesResponseType(200,Type=typeof(Pokemon))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemon(int pokeId)
+        {
+            if (!_pokemonRepository.PokemonExists(pokeId))
+                return NotFound();
+
+            var pokemon = _pokemonRepository.GetPokemon(pokeId);
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemon);
+        }
+
+
+
+    }
+}
---
+++ Interfaces/IPokemonRepository.cs
@@ -0,0 +1,15 @@
+﻿using PokemonReviewAPI.Models;
+
+namespace PokemonReviewAPI.Interfaces
+{
+    public interface IPokemonRepository
+    {
+        ICollection<Pokemon> GetPokemons();
+        Pokemon GetPokemon(int id);
+        Pokemon GetPokemon(string name);
+        decimal GetPokemonRating(int pokeId);
+        bool PokemonExists(int pokeId);
+
+
+    }
+}
